34540439d75f9f5cd775c1637c317540a4fa08cc
Martin Chapman <contact@martinchapman.co.uk>
2024-09-02 11:37:05 +0100

    Update made by Phenoflow

---
+++ python/proliferative-type-1-diabetese---secondary.py
@@ -0,0 +1,25 @@
+# phekb, 2024.
+
+import sys, csv, re
+
+codes = [{"code":"1567945","system":"ICD10CM"},{"code":"1567946","system":"ICD10CM"},{"code":"1567947","system":"ICD10CM"},{"code":"1567948","system":"ICD10CM"},{"code":"45537958","system":"ICD10CM"},{"code":"45561947","system":"ICD10CM"},{"code":"45571654","system":"ICD10CM"},{"code":"45576437","system":"ICD10CM"},{"code":"45591026","system":"ICD10CM"},{"code":"45595793","system":"ICD10CM"},{"code":"45595794","system":"ICD10CM"},{"code":"45605397","system":"ICD10CM"},{"code":"1567945","system":"ICD10CM"},{"code":"45595793","system":"ICD10CM"},{"code":"45591026","system":"ICD10CM"},{"code":"1567946","system":"ICD10CM"},{"code":"45561947","system":"ICD10CM"},{"code":"45605397","system":"ICD10CM"},{"code":"1567947","system":"ICD10CM"},{"code":"45595794","system":"ICD10CM"},{"code":"45537958","system":"ICD10CM"},{"code":"1567948","system":"ICD10CM"},{"code":"45576437","system":"ICD10CM"},{"code":"45571654","system":"ICD10CM"},{"code":"1567945","system":"ICD10CM"},{"code":"1567946","system":"ICD10CM"},{"code":"1567947","system":"ICD10CM"},{"code":"1567948","system":"ICD10CM"},{"code":"45537958","system":"ICD10CM"},{"code":"45561947","system":"ICD10CM"},{"code":"45571654","system":"ICD10CM"},{"code":"45576437","system":"ICD10CM"},{"code":"45591026","system":"ICD10CM"},{"code":"45595793","system":"ICD10CM"},{"code":"45595794","system":"ICD10CM"},{"code":"45605397","system":"ICD10CM"},{"code":"1567945","system":"ICD10CM"},{"code":"45595793","system":"ICD10CM"},{"code":"45591026","system":"ICD10CM"},{"code":"1567946","system":"ICD10CM"},{"code":"45561947","system":"ICD10CM"},{"code":"45605397","system":"ICD10CM"},{"code":"1567947","system":"ICD10CM"},{"code":"45595794","system":"ICD10CM"},{"code":"45537958","system":"ICD10CM"},{"code":"1567948","system":"ICD10CM"},{"code":"45576437","system":"ICD10CM"},{"code":"45571654","system":"ICD10CM"}];
+REQUIRED_CODES = 1;
+with open(sys.argv[1], 'r') as file_in, open('type-1-diabetese-potential-cases.csv', 'w', newline='') as file_out:
+    csv_reader = csv.DictReader(file_in)
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["proliferative-type-1-diabetese---secondary-identified"])
+    csv_writer.writeheader();
+    codes_identified = 0;
+    for row in csv_reader:
+        newRow = row.copy();
+        for cell in row:
+            # Iterate cell lists (e.g. codes)
+            for item in re.findall(r'\(([^,]*)\,', row[cell]):
+                if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
+                if(codes_identified>=REQUIRED_CODES):
+                    newRow["proliferative-type-1-diabetese---secondary-identified"] = "CASE";
+                    break;
+            if(codes_identified>=REQUIRED_CODES): break;
+        if(codes_identified<REQUIRED_CODES):
+            newRow["proliferative-type-1-diabetese---secondary-identified"] = "UNK";
+        codes_identified=0;
+        csv_writer.writerow(newRow)
